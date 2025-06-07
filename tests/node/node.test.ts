ef4e02918eaebfd7213f6b2cf4f5729f255b75d7
Xvezda <xvezda@naver.com>
2025-06-07 11:39:50 +0900

    Update fs types

--- tests/node/node.test.ts
+++ tests/node/node.test.ts
@@ -13,6 +13,116 @@ const ruleTester = new RuleTester({
   },
 });
 
+// Test for fs module functions that should have @throws documentation
+ruleTester.run(
+  'fs-sync-functions-throws-documentation',
+  plugin.rules['no-undocumented-throws'],
+  {
+    valid: [
+      // Test functions that already have proper @throws documentation
+      {
+        code: `
+          /**
+           * @throws {NodeJS.ErrnoException} When file operations fail
+           */
+          function testRename() {
+            const fs = require('fs');
+            fs.renameSync('old.txt', 'new.txt');
+          }
+        `,
+      },
+      {
+        code: `
+          /**
+           * @throws {NodeJS.ErrnoException} When file operations fail
+           */
+          function testReadFile() {
+            const fs = require('fs');
+            return fs.readFileSync('test.txt');
+          }
+        `,
+      },
+      {
+        code: `
+          /**
+           * @throws {NodeJS.ErrnoException} When file operations fail
+           */
+          function testWriteFile() {
+            const fs = require('fs');
+            fs.writeFileSync('test.txt', 'data');
+          }
+        `,
+      },
+      {
+        code: `
+          /**
+           * @throws {NodeJS.ErrnoException} When file operations fail
+           */
+          function testUnlink() {
+            const fs = require('fs');
+            fs.unlinkSync('test.txt');
+          }
+        `,
+      },
+      {
+        code: `
+          /**
+           * @throws {NodeJS.ErrnoException} When directory operations fail
+           */
+          function testMkdir() {
+            const fs = require('fs');
+            fs.mkdirSync('testdir');
+          }
+        `,
+      },
+      {
+        code: `
+          /**
+           * @throws {NodeJS.ErrnoException} When copy operations fail
+           */
+          function testCopy() {
+            const fs = require('fs');
+            fs.copyFileSync('src.txt', 'dest.txt');
+          }
+        `,
+      },
+      {
+        code: `
+          /**
+           * @throws {NodeJS.ErrnoException} When access checks fail
+           */
+          function testAccess() {
+            const fs = require('fs');
+            fs.accessSync('test.txt');
+          }
+        `,
+      },
+    ],
+    invalid: [
+      // Test more basic case - JSON.parse with dynamic input should fail without @throws
+      {
+        code: `
+          function testJsonParseMissingThrows(input: string) {
+            return JSON.parse(input);
+          }
+        `,
+        output: `
+          /**
+           * @throws {SyntaxError}
+           */
+          function testJsonParseMissingThrows(input: string) {
+            return JSON.parse(input);
+          }
+        `,
+        errors: [{
+          messageId: 'missingThrowsTag'
+        }],
+      },
+    ],
+  }
+);
+
+// Test for original functionality
 ruleTester.run(
   'no-undocumented-throws',
   plugin.rules['no-undocumented-throws'],
